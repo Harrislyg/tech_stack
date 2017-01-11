260468f73aa73cccc6953aa84912c01f630f71c3
HarrisLeow <leow_harris@hotmail.sg>
2017-01-11 02:01:51 -0800

    link reducer with action with touch table to render list info onclick

--- src/reducers/SelectionReducer.js
+++ src/reducers/SelectionReducer.js
@@ -1,5 +1,10 @@
-// Redux reducer has to return something. Canot return undefined
-export default (state, action) => {
-  console.log(action)
-  return null
+// Redux reducer has to return something. Cannot return undefined
+export default (state = null, action) => {
+  switch (action.type) {
+    case 'select_library':
+      console.log(action.payload)
+      return action.payload
+    default:
+      return state
+  }
 }
